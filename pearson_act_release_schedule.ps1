229ed470a5adffe8a177f852afa94538309e3970
Shane <shane.omalley-potting@uwrf.edu>
2018-05-31 10:53:28 -0500

    Fixing October Discrepency
    The recently fixed logic for including the 21st as a third Saturday caused in issue with October where the third Saturday report was delayed due to a later testing date.

--- pearson_act_release_schedule.ps1
+++ pearson_act_release_schedule.ps1
@@ -8,7 +8,7 @@
     Values will need to be updated annually
 #>
 $schedule = @{
-	"holidays" = @((Get-Date "09-04-2017"), (Get-Date "11-24-2017"), (Get-Date "12-25-2017"), (Get-Date "01-01-2018"), (Get-Date "01-15-2018"), (Get-Date "09-01-2018"), (Get-Date "05-28-2018"), (Get-Date "07-04-2018"));;
+	"holidays" = @((Get-Date "09-04-2017"),(Get-Date "10-21-2017"),(Get-Date "11-24-2017"), (Get-Date "12-25-2017"), (Get-Date "01-01-2018"), (Get-Date "01-15-2018"), (Get-Date "09-01-2018"), (Get-Date "05-28-2018"), (Get-Date "07-04-2018"));;
 	"special_runs" = @((Get-Date "09-16-2017"),(Get-Date "11-11-2017"), (Get-Date "07-21-2018"));
 	"start_date" = "09/06/2017";
 	"start_code" = 71110
